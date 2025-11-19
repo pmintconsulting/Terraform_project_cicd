63737a537a7a2b6871ba56e3aa6744d96fede781
pmintconsulting <pmintcollectconsulting@gmail.com>
2025-11-19 14:16:13 +0000

    added a new change to security group

--- day2-code/security-groups.tf
+++ day2-code/security-groups.tf
@@ -1,7 +1,7 @@
-resource "aws_secruity_group" "sunil-sec-groups" {
+resource "aws_security_group" "sunil-sec-groups" {
   name = "sunil_allow_rules"
   description = "Allow TLS inbound traffice and all outbound traffic"
-  vpc_id = data.aws_vpc.sunil-existing-vpc.vpc_id
+  vpc_id = data.aws_vpc.sunil-existing-vpc.id
 
   tags = {
     Name = "sunil_firewall_rules"
@@ -9,7 +9,7 @@ resource "aws_secruity_group" "sunil-sec-groups" {
 }
 #allow all outgoing ports for IPV4
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_secruity_group.sunil_sec-groups.id
+    security_group_id = aws_security_group.sunil-sec-groups.id
     cidr_ipv4 = "0.0.0.0/0"
     ip_protocol = "-1" #semantically equivalent to all ports
     from_port   = 22
