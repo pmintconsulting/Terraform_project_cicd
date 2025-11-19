ed1972ee3536b725ac07fd387152b9519c7cc336
pmintconsulting <pmintcollectconsulting@gmail.com>
2025-11-19 17:15:00 +0000

    Added change for VM name

--- day2-code/ec2.tf
+++ day2-code/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   instance_type          = var.machine_size
   key_name               = var.my_private_key_name
   vpc_security_group_ids = [aws_security_group.sunil-sec-groups.id]
-  
+
   # adding security group to ec2 vm(ws)
   tags = {
     Name = var.vm_name
